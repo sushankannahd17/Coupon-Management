2d75d0abd93ed73e57d4eb21e7d0232e15ef4b98
Sushan Kannah D <sushan.kd17@gmail.com>
2025-11-24 19:48:35 +0530

    Added CORS for integration

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const appRouter = require("./src/app");
-require("dotenv").config()
-
-const uri = ""
+require("dotenv").config();
+const cors = require("cors");
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
@@ -12,6 +11,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 const app = express();
 
+app.use(cors());
 app.use(express.json())
 app.use("/api", appRouter);
 
